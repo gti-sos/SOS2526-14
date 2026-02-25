f7c1185308d2c4ce884fadc80fdf38e5e985f532
Jeremías <jrmspls@gmail.com>
2026-02-25 09:00:53 +0100

    Error en ruta

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const calcularFRB = require("./index-FRB.js"); // algoritmo Fernando
-const calcularJPC = require("./index-jpc.js"); // algoritmo Jeremias
+const calcularJPC = require("./index-JPC.js"); // algoritmo Jeremias
 const spaceLaunchesAPI = require("./api/spaceLaunches");
 
 const app = express();
